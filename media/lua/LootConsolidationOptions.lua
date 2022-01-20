d8111fa23ff20f6de029b0fc0b1d6d16990621f4
Brandon <beemerwt@gmail.com>
2022-01-20 15:01:39 -0600

    Decided on (faster) zombie consolidation

--- media/lua/LootConsolidationOptions.lua
+++
@@ -1,8 +0,0 @@
-
-LootConsolidation = _G['LootConsolidation'] or {}
-
- -- Should remove zombies after they've been consolidated?
- LootConsolidation.shouldRemove = false;
-
- -- What shouldn't be consolidated? Can be an item name or type
-LootConsolidation.consolidationFilters = { "Base.Stake", "Clothing" };
--- media/lua/server/LootConsolidation.lua
+++ media/lua/server/LootConsolidation.lua
@@ -1,43 +1,117 @@
 
 LootConsolidation = _G['LootConsolidation'] or {}
 
-if isClient() then return end
-local Commands = {}
+LootConsolidation.Options = {
+  -- Should remove zombies after they've been consolidated?
+  shouldRemove = true,
 
-function Commands.getDeadZombieItemsInCell(cell)
-  local zombies = cell:getZombieList();
+  -- Search radius every time a zombie is killed
+  radius = 2,
 
-  for i=0,zombies:size()-1 do
-    if zombies:get(i):isDead() then
-      return "Consolidated";
+  -- Should the items consolidate by dropping to the "Floor"
+  --  otherwise, they consolidate to a specific zombie.
+  dropToFloor = true,
+
+  -- Should we consolidate only items dropped from zombies
+  onlyZombies = true,
+
+  -- What shouldn't be consolidated? Can be an item name or type
+  -- All "Clothing" is, by default, not consolidated.
+  consolidationFilters = { "Stake", "Clothing", "nil" },
+}
+
+function LootConsolidation.isValidItem(item)
+  print(tostring(item:getName()) .. " Category " .. tostring(item:getDisplayCategory()));
+  for _,type in ipairs(LootConsolidation.Options.consolidationFilters) do
+    if item:getDisplayCategory() == nil or item:getDisplayCategory() == type or item:getName() == type then
+      return false
     end
   end
+
+  return true
 end
 
-function Commands.requestItems(player, args)
-  local test = LootConsolidation.getDeadZombieItemsInCell(player:getCell());
-  print(test);
-end
-
--- Params:
--- IsoZombie zombie (extends IsoGameCharacter, IsoMovingObject, IsoObject)
-function LootConsolidation.OnZombieDead(zombie, args)
-  print("LootConsolidation OnZombieDead");
-  print("Zombie: " .. tostring(zombie))
-
-  local cell = zombie:getCell();
-  local items = LootConsolidation.getDeadZombieItemsInCell(cell)
-end
-
-function LootConsolidation.OnClientCommand(module, command, player, args)
-  if module ~= "LootConsolidation" then return end
-
-  print("Client command dispatched for LootConsolidation");
-
-  if Commands[command] then
-    Commands[command](player, args)
+function LootConsolidation.transferItem(item, srcContainer, destContainer)
+  if isClient() then
+    destContainer:addItemOnServer(item);
+    srcContainer:removeItemOnServer(item);
+  else
+    destContainer:addItem(item);
+    srcContainer:Remove(item);
   end
+
+  item:setAttachedSlot(-1);
+  item:setAttachedSlotType(nil);
+  item:setAttachedToModel(nil);
+
+  srcContainer:setDrawDirty(true);
+  srcContainer:setHasBeenLooted(true);
+  destContainer:setDrawDirty(true);
 end
+
+function LootConsolidation.consolidateTo(zombie, radius)
+  local cell = zombie:getCell();
+  local sqs = {}
+
+  local cx = zombie:getX();
+  local cy = zombie:getY();
+  local cz = zombie:getZ();
+
+  for dy=-radius,radius do
+    for dx=-radius,radius do
+      local square = cell:getGridSquare(cx + dx, cy + dy, cz)
+      if square then
+        table.insert(sqs, square)
+      end
+    end
+  end
+
+  local destContainer = zombie:getInventory();
+
+  for _,gs in ipairs(sqs) do
+    -- stop moving items thru walls...
+    local currentSq = zombie:getCurrentSquare()
+    if gs ~= currentSq and currentSq and currentSq:isBlockedTo(gs) then
+      gs = nil
+    end
+
+    if gs ~= nil then
+      local deadBodies = gs:getDeadBodys();
+      if deadBodies ~= nil then
+        if deadBodies:size() ~= 0 then
+          for i = 1, deadBodies:size() do
+            local deadBody = deadBodies:get(i-1);
+            if deadBody:isZombie() or not LootConsolidation.Options.onlyZombies then
+              local srcContainer = deadBody:getContainer();
+              local x = 0
+              
+              while x < srcContainer:getItems():size() do
+                local item = srcContainer:getItems():get(x)
+                if LootConsolidation.isValidItem(item) then
+                  LootConsolidation.transferItem(item, srcContainer, destContainer);
+                else
+                  x = x + 1
+                end
+              end
+
+              if LootConsolidation.Options.shouldRemove then
+                deadBody:removeFromSquare();
+                deadBody:removeFromWorld();
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  print("Loot Consolidated")
+end
+
+function LootConsolidation.OnZombieDead(zombie)
+  local radius = LootConsolidation.Options.radius or 1;
+  LootConsolidation.consolidateTo(zombie, radius);
+end
+
 
 Events.OnZombieDead.Add(LootConsolidation.OnZombieDead)
-Events.OnClientCommand.Add(LootConsolidation.OnClientCommand)
